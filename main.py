e21235cbb1c051d0bd9ce15d944c8bd8f833363f
Cristian Jetomo <120572492+ji-chani@users.noreply.github.com>
2026-07-15 00:46:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CBJ Portfolio",
+    layout="wide"
+)
+
+pages = {
+    "About Me": [
+        st.Page("./pages/1.1_Intro.py", title="Intro"),
+        st.Page("./pages/1.2_Education_Experience.py", title="Education and Experiences"),
+        st.Page("./pages/1.3_Recognition_Awards.py", title="Recognition and Awards"),
+    ],
+    "Things I Do": [
+        st.Page("./pages/2.1_Research.py", title="Research"),
+        # st.Page("./pages/4_Activities.py", title="Activities")
+    ]
+}
+
+pg = st.navigation(pages, position='top')
+pg.run()
